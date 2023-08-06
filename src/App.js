fd159c84f01faa2c41121a15d8fb74ad94c24573
AbhishekBhure <abhiab7411@gmail.com>
2023-08-06 23:52:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import GameContainer from "./components/GameContainer";
 
 function App() {
   return (
-    <Grid container={true}>
-      <Grid item xs={12}>
-        <Box sx={{ padding: "16px" }}>
-          <Typography variant="h4" align="center">
-            Memory Card
-          </Typography>
-        </Box>
+    <div>
+      <Grid container={true}>
+        <Grid item xs={12}>
+          <Box sx={{ padding: "16px" }}>
+            <Typography variant="h4" align="center">
+              Memory Card
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={1} md={2} lg={3} />
+        <Grid item xs={10} md={8} lg={6}>
+          <GameContainer />
+        </Grid>
+        <Grid item xs={1} md={2} lg={3} />
       </Grid>
-      <Grid item xs={1} md={2} lg={3} />
-      <Grid item xs={10} md={8} lg={6}>
-        <GameContainer />
-      </Grid>
-      <Grid item xs={1} md={2} lg={3} />
-    </Grid>
+    </div>
   );
 }
 
