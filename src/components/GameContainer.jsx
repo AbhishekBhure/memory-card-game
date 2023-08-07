bf195e48bdb07000ed5f3efbc9ebf01f5b6406ff
AbhishekBhure <abhiab7411@gmail.com>
2023-08-07 23:50:07 +0530

    started with gameDisplay.js

--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,33 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GameDisplay from "./GameDisplay";
 import arrayShuffle from "array-shuffle";
-const gameContainer = () => {
-  const types = ["❤", "♋", "🕉", "💥", "👦", "🤩", "🌮", "🛺"];
 
-  const getGameData = () => {
-    //get types and load the types in an new array x2(as an object)
-    const gameData = [];
+const types = ["❤", "♋", "🕉", "💥", "👦", "🤩", "🌮", "🛺"];
+const getGameData = () => {
+  //get types and load the types in an new array x2(as an object)
+  const gameData = [];
 
-    const typesForGameData = [...types, ...types];
+  const typesForGameData = [...types, ...types];
 
-    //shuffle  the object
-    let shuffledTypes = arrayShuffle(typesForGameData);
+  //shuffle  the object
+  let shuffledTypes = arrayShuffle(typesForGameData);
 
-    shuffledTypes.forEach((type, index) => {
-      gameData.push({
-        type,
-        id: index,
-        open: false,
-        remove: false,
-      });
+  shuffledTypes.forEach((type, index) => {
+    gameData.push({
+      type,
+      id: index,
+      open: false,
+      remove: false,
     });
+  });
 
-    //return the array with the 16 cards
-    console.log(gameData);
-  };
-
-  getGameData();
-  return <GameDisplay />;
+  //return the array with the 16 cards
+  return gameData;
+};
+const GameContainer = () => {
+  const [cards, setCards] = useState(getGameData());
+  return <GameDisplay cards={cards} />;
 };
 
-export default gameContainer;
+export default GameContainer;
--- src/components/GameDisplay.jsx
+++ src/components/GameDisplay.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Grid, Box, Paper } from "@mui/material";
+import ReactCardFlip from "react-card-flip";
 
-const gameDisplay = () => {
-  return <div>gameDisplay</div>;
+const GameDisplay = ({ cards }) => {
+  return (
+    <Grid container>
+      <Grid item>
+        <ReactCardFlip isFlipped={false}>
+          <Paper>
+            <Box
+              sx={{ height: 100, cursor: "pointer", background: "gray" }}
+              display="flex"
+            >
+              Front
+            </Box>
+          </Paper>
+          <Paper>
+            <Box>Back</Box>
+          </Paper>
+        </ReactCardFlip>
+      </Grid>
+      <Grid item></Grid>
+    </Grid>
+  );
 };
 
-export default gameDisplay;
+export default GameDisplay;
